523f2d6033b9153e4e4ab1d6a4f3bce6af4509eb
Marton Hun <hunmarton@hotmail.com>
2020-08-14 19:28:21 +0200

    Added Local storage

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("todoInput");
   const btn = document.getElementById("todoButton");
   const container = document.getElementById("todoContainer");
-  
 
   const getInputValue = () => {
       const inputVal = input.value;
@@ -34,17 +33,63 @@ window.addEventListener("DOMContentLoaded", () => {
     removeItem(e);
   };
   
+  const setLocalStorage = (todo) =>{
+    let todos;
+    if(localStorage.getItem('todos') === null){
+      todos = [];
+    }else{
+      todos= JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    };
+  const getLocalStorage = () =>{
+    let todos;
+    if(localStorage.getItem('todos') === null){
+      todos = [];
+    }else{
+      todos= JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(todo =>{
+      let html = `
+    <div class="todo-container">
+      <div class="todo-single">
+        <p>${todo}</p>
+        <div class="icons">
+            <i class="fas fa-pen"></i>
+            <i class="far fa-trash-alt"></i>
+        </div>
+      </div>
+    </div>`;
+    container.innerHTML += html;
+    });    
+  }  
+  const deleteLocalStorage = (e)=>{
+    let todos;
+    if(localStorage.getItem('todos') === null){
+      todos = [];
+    }else{
+      todos= JSON.parse(localStorage.getItem('todos'));
+    }
+    let ind= e.target.parentElement.parentElement.firstChild.nextSibling.innerHTML;
+    todos.splice(todos.indexOf(ind), 1);
+    localStorage.setItem('todos',JSON.stringify(todos));
+  };
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     if(input.value != ''){
-        createHtml();
+      setLocalStorage(getInputValue());  
+      createHtml();  
     }
   });
   container.addEventListener('click', (e)=>{
     if(e.target.classList.contains('fa-trash-alt')){
         removeItem(e);
+        deleteLocalStorage(e);
     }else if(e.target.classList.contains('fa-pen')){
         editItem(e);
+        deleteLocalStorage(e);
     }    
   });  
+getLocalStorage();
 });
